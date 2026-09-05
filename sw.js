a01eca2c5e78e333733e4f71fee63910f220ae01
kaka74mk-jpg <kaka74mk@gmail.com>
2026-09-05 13:02:21 +0330

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,18 +1,16 @@
-const CACHE_NAME = "mk-studio-v1";
+const CACHE_NAME = "mk-studio-v2";
 
 const APP_SHELL = [
   "./",
   "./index.html",
   "./manifest.webmanifest",
-  "./icons/icon-192.png",
-  "./icons/icon-512.png"
+  "./icon-192.png",
+  "./icon-512.png"
 ];
 
 self.addEventListener("install", (event) => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      return cache.addAll(APP_SHELL);
-    })
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(APP_SHELL))
   );
 
   self.skipWaiting();
@@ -20,13 +18,13 @@ self.addEventListener("install", (event) => {
 
 self.addEventListener("activate", (event) => {
   event.waitUntil(
-    caches.keys().then((keys) => {
-      return Promise.all(
+    caches.keys().then((keys) =>
+      Promise.all(
         keys
           .filter((key) => key !== CACHE_NAME)
           .map((key) => caches.delete(key))
-      );
-    })
+      )
+    )
   );
 
   self.clients.claim();
@@ -36,8 +34,8 @@ self.addEventListener("fetch", (event) => {
   if (event.request.method !== "GET") return;
 
   event.respondWith(
-    caches.match(event.request).then((cachedResponse) => {
-      return cachedResponse || fetch(event.request);
-    })
+    caches.match(event.request).then(
+      (cachedResponse) => cachedResponse || fetch(event.request)
+    )
   );
 });
