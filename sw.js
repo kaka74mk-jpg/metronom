5a1cb261a80c0fa48e07365136c41ab21367f832
kaka74mk-jpg <kaka74mk@gmail.com>
2026-09-05 12:49:09 +0330

    Add files via upload
    Update PWA files and icons

--- sw.js
+++ sw.js
@@ -3,7 +3,9 @@ const CACHE_NAME = "mk-studio-v1";
 const APP_SHELL = [
   "./",
   "./index.html",
-  "./manifest.webmanifest"
+  "./manifest.webmanifest",
+  "./icons/icon-192.png",
+  "./icons/icon-512.png"
 ];
 
 self.addEventListener("install", (event) => {
